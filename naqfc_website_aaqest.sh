94d5820fc44292992a6e8809805e2c9d82df7890
drnimbusrain <patrickc@aaqest.arl.noaa.gov>
2019-04-25 14:37:27 -0400

    updated spatial plotting script

--- naqfc_website_aaqest.sh
+++ naqfc_website_aaqest.sh
@@ -2,19 +2,21 @@
 
 
 export OMP_NUM_THREADS=2
-# get the current date
+# get the current date (loop through current, or as far back as needed to fill)
 date
-yyyymmdd=$(date -d "-112 days" +%Y-%m-%d)
-yyyymmddform=$(date -d "-112 days" +%Y%m%d)
-yyyymmdd72hr=$(date -d "-109 days" +%Y-%m-%d)
+yyyymmdd=$(date -d "-2 days" +%Y-%m-%d)
+yyyymmdd48hr=$(date -d "-0 days" +%Y-%m-%d)
+yyyymmddform=$(date -d "-2 days" +%Y%m%d)
 
 # go to data directory
-data_dir=/data/aqf/patrickc/CMAQ_DATA/cmaq5.0.2-72hr
-#data_dir=/gpfs/hps2/ptmp/Patrick.C.Campbell/fengsha/gfs.$yyyymmdd/00
+data_dir=/data/aqf3/patrickc/5xpm
+mkdir $data_dir/$yyyymmddform
 cd $data_dir
-
+ln -sf /data/aqf2/barryb/5xpm/$yyyymmddform/* $yyyymmddform/.
+ 
 # netcdf files
-files="aqm.${yyyymmddform}.t12z.aconc-pm25.ncf"
+files=$yyyymmddform/"aqm.t12z.aconc-sfc.ncf"
+files_met=$yyyymmddform/"aqm.t12z.metcro2d.ncf"
 
 #naqfc verify scripts 
 pair=/data/aqf/patrickc/naqfc_verify_scripts/01.verify_pair.py
@@ -25,34 +27,45 @@ spatial=/data/aqf/patrickc/naqfc_verify_scripts/05.verify_spatial_plots.py
 box=/data/aqf/patrickc/naqfc_verify_scripts/06.verify_box_plots.py
 
 # pair the data
-#${pair} -f ${files} 
+${pair} -f ${files}     -s 'OZONE' 'PM2.5' 'PM10' 'CO' 'NO' 'NO2' 'NOX' 'NO2Y' 
+mv AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair.hdf AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_chem.hdf
+${pair} -f ${files_met} -s 'BARPR' 'TEMP' 'WS' 'WD' 'SRAD' 'RHUM'
+mv AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair.hdf AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_met.hdf 
 
+conda activate nco_stable
+ncap2 -A -s "RT=RC+RN" $files_met $yyyymmddform/aqm.t12z.precip.ncf
+deactivate
 
-
-
+${pair} -f ${files_met} -s 'PRECIP'
+mv AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair.hdf AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_precip.hdf
 
 # create taylor
-${taylor} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd72hr}-12_pair.hdf -s 'OZONE' 'PM2.5' -n ${yyyymmddform}
+${taylor} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_chem.hdf    -s 'OZONE' 'PM2.5' 'PM10' 'CO' 'NO' 'NO2' 'NOX' 'NO2Y' -n ${yyyymmddform}
+${taylor} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_met.hdf     -s 'BARPR' 'TEMP' 'WS' 'WD' 'SRAD' 'RHUM' 'PRECIP'     -n ${yyyymmddform}
+${taylor} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_precip.hdf  -s 'PRECIP'                                            -n ${yyyymmddform}
 for i in 'R1' 'R2' 'R3' 'R4' 'R5' 'R6' 'R7' 'R8' 'R9' 'R10'; do
   echo ${i}
-  ${taylor} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd72hr}-12_pair.hdf -s 'OZONE' 'PM2.5' -b True -e ${i} -n ${yyyymmddform}
+  ${taylor} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_chem.hdf -s 'OZONE' 'PM2.5' 'PM10' 'CO' 'NO' 'NO2' 'NOX' 'NO2Y' -b True -e ${i} -n ${yyyymmddform}
+  ${taylor} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_met.hdf  -s 'BARPR' 'TEMP' 'WS' 'WD' 'SRAD' 'RHUM' 'PRECIP'     -b True -e ${i} -n ${yyyymmddform}
 done
 
-
-
 # spatial bias plots
-${bias} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd72hr}-12_pair.hdf -s 'OZONE' 'PM2.5' -n ${yyyymmddform}
+${bias} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_chem.hdf -s 'OZONE' 'PM2.5' 'PM10' 'CO' 'NO' 'NO2' 'NOX' 'NO2Y' -n ${yyyymmddform}
+${bias} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_met.hdf  -s 'BARPR' 'TEMP' 'WS' 'WD' 'SRAD' 'RHUM' 'PRECIP'     -n ${yyyymmddform}
 for i in 'R1' 'R2' 'R3' 'R4' 'R5' 'R6' 'R7' 'R8' 'R9' 'R10'; do
   echo ${i}
-  ${bias} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd72hr}-12_pair.hdf -s 'OZONE' 'PM2.5'  -b True -e ${i} -n ${yyyymmddform}
+  ${bias} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_chem.hdf -s 'OZONE' 'PM2.5' 'PM10' 'CO' 'NO' 'NO2' 'NOX' 'NO2Y'  -b True -e ${i} -n ${yyyymmddform}
+  ${bias} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_met.hdf  -s 'BARPR' 'TEMP' 'WS' 'WD' 'SRAD' 'RHUM' 'PRECIP'      -b True -e ${i} -n ${yyyymmddform}
 done
 
 
 # spatial overlay plots
-${spatial} -f ${files} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd72hr}-12_pair.hdf -s 'OZONE','PM2.5' -n ${yyyymmddform}
+${spatial} -f ${files} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_chem.hdf -s 'OZONE' 'PM2.5' 'PM10' 'CO' 'NO' 'NO2' 'NOX' 'NO2Y' -n ${yyyymmddform}
+${spatial} -f ${files} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_met.hdf  -s 'BARPR' 'TEMP' 'WS' 'WD' 'SRAD' 'RHUM' 'PRECIP'     -n ${yyyymmddform}
 for i in 'R1' 'R2' 'R3' 'R4' 'R5' 'R6' 'R7' 'R8' 'R9' 'R10'; do
   echo ${i}
-  ${spatial} -f ${files} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd72hr}-12_pair.hdf -s  'OZONE' 'PM2.5' -b True -e ${i} -n ${yyyymmddform}
+  ${spatial} -f ${files} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_chem.hdf -s  'OZONE' 'PM2.5' 'PM10' 'CO' 'NO' 'NO2' 'NOX' 'NO2Y' -b True -e ${i} -n ${yyyymmddform}
+  ${spatial} -f ${files} -p AIRNOW_CMAQ_${yyyymmdd}-13_${yyyymmdd48hr}-12_pair_met.hdf  -s  'BARPR' 'TEMP' 'WS' 'WD' 'SRAD' 'RHUM' 'PRECIP'     -b True -e ${i} -n ${yyyymmddform}
 done
 
 
@@ -60,26 +73,29 @@ done
 # make GIFS
 ##########################################################################################################
 for i in '5X' 'R1' 'R2' 'R3' 'R4' 'R5' 'R6' 'R7' 'R8' 'R9' 'R10'; do
-  for j in 'OZONE' 'PM2P5'; do
+  for j in 'OZONE' 'PM2P5' 'PM10' 'CO' 'NO' 'NO2' 'NOX' 'NO2Y' 'BARPR' 'TEMP' 'WS' 'WD' 'SRAD' 'RHUM' 'PRECIP'; do
     echo "${i}.${j}"
     convert -delay 40 -loop 0 ${yyyymmddform}.${i}.${j}.spbias.*.jpg ${yyyymmddform}.${i}.${j}.spbias.gif
   done
 done
 
 for i in '5X' 'R1' 'R2' 'R3' 'R4' 'R5' 'R6' 'R7' 'R8' 'R9' 'R10'; do
-  for j in 'OZONE' 'PM2P5'; do
+  for j in 'OZONE' 'PM2P5' 'PM10' 'CO' 'NO' 'NO2' 'NOX' 'NO2Y' 'BARPR' 'TEMP' 'WS' 'WD' 'SRAD' 'RHUM' 'PRECIP'; do
     echo "${i}.${j}"
     convert -delay 40 -loop 0 ${yyyymmddform}.${i}.${j}.sp.*.jpg ${yyyymmddform}.${i}.${j}.sp.gif
   done
 done
 
 for i in '5X' 'R1' 'R2' 'R3' 'R4' 'R5' 'R6' 'R7' 'R8' 'R9' 'R10'; do
-  for j in 'OZONE' 'PM2P5'; do
+  for j in 'OZONE' 'PM2P5' 'PM10' 'CO' 'NO' 'NO2' 'NOX' 'NO2Y' 'BARPR' 'TEMP' 'WS' 'WD' 'SRAD' 'RHUM' 'PRECIP'; do
     echo "${i}.${j}"
     convert -delay 40 -loop 0 ${yyyymmddform}.${i}.${j}.tyr.*.jpg ${yyyymmddform}.${i}.${j}.tyr.gif
   done
 done
 ##########################################################################################################
+
+mv *.jpg /data/aqf2/testbed-bak/www/testbed/TCHAI/hkim/${yyyymmddform}/
+mv *.gif /data/aqf2/testbed-bak/www/testbed/TCHAI/hkim/${yyyymmddform}/
 
 # Transfer data back to aaqest
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
