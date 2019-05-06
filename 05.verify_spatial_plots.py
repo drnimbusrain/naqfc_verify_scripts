bff643653020404237e4df48bad37814ce2e0928
drnimbusrain <patrickc@aaqest.arl.noaa.gov>
2019-05-06 14:27:39 -0400

    updated spatial plot color scheme

--- 05.verify_spatial_plots.py
+++ 05.verify_spatial_plots.py
@@ -86,9 +86,9 @@ def make_spatial_plot(da, df, outname, proj, startdate, enddate, region='domain'
      latmin,lonmin,latmax,lonmax,acro = get_epa_bounds(index=None,acronym=region)
     
     extent = [lonmin,lonmax,latmin,latmax]
-    ax = da.monet.quick_map(cbar_kwargs=cbar_kwargs, figsize=(15, 8), map_kwarg={'states': True, 'crs': proj,'extent':extent},robust=True) 
+    ax = da.monet.quick_map(cbar_kwargs=cbar_kwargs, figsize=(15, 8), map_kwarg={'states': True, 'crs': proj,'extent':extent},robust=True,cmap=plt.cm.get_cmap('jet')) 
     plt.gcf().canvas.draw() 
-    plt.tight_layout(pad=-1) 
+    plt.tight_layout(pad=-1)
     if startdate == None and enddate == None:
      date = pd.Timestamp(da.time.values) 
      dt = date - initial_datetime
@@ -101,7 +101,7 @@ def make_spatial_plot(da, df, outname, proj, startdate, enddate, region='domain'
     vmin, vmax = cbar.get_ybound() 
     vars = df.keys() 
     varname = [x for x in vars if x not in ['latitude','longitude']][0] 
-    ax.scatter(df.longitude.values,df.latitude.values,s=25,c=df[varname],transform=ccrs.PlateCarree(),edgecolor='b',linewidth=.50,vmin=vmin,vmax=vmax) 
+    ax.scatter(df.longitude.values,df.latitude.values,s=25,c=df[varname],transform=ccrs.PlateCarree(),edgecolor='b',linewidth=.50,vmin=vmin,vmax=vmax,cmap=plt.cm.get_cmap('jet'))
     ax.set_extent(extent,crs=ccrs.PlateCarree())
     
     if startdate == None and enddate == None:
