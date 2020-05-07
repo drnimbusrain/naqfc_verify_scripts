dc4b22aaaebcdac3031dbeaeec248207c3ede4dc
drnimbusrain <patrickc@aaqest.arl.noaa.gov>
2020-05-07 13:27:51 -0400

    Updated scripts capabilities (10.combine now can average.)

--- 10.verify_combine_pair.py
+++ 10.verify_combine_pair.py
@@ -18,7 +18,6 @@ import monet
 from monet.util.tools import long_to_wide
 import pandas as pd
 
-
 if __name__ == '__main__':
     parser = ArgumentParser(
         description='combines paired data files for multiple model runs',
@@ -37,7 +36,7 @@ if __name__ == '__main__':
         type=str,
         nargs='+',
         required=False,
-        default=['O3', 'PM25_TOT'])
+        default=['OZONE'])
     parser.add_argument(
         '-mdf',
         '--mergedf',
@@ -53,21 +52,35 @@ if __name__ == '__main__':
         required=False,
         default='./AIRNOW_CMAQ_merged_pair')
     parser.add_argument(
+        '-avg',
+        '--averagef',
+        help='boolean operator to average entire dataframes (slow)',
+        type=bool,
+        required=False,
+        default=False)
+    parser.add_argument(
         '-v',
         '--verbose',
         help='print debugging information',
         action='store_true',
-        required=False)
+        required=False,
+        default=False)
     args = parser.parse_args()
 
     paired_files = args.paired_files
     species = args.model_species
     output = args.output
-    verbose = args.verbose
-    mergedf    = args.mergedf
-
+    mergedf = args.mergedf
+    avg = args.averagef
+    
+    mapping_table = {'OZONE':'O3', 'PM2.5':'PM25_TOT', 'PM10':'PMC_TOT', 'CO':'CO_new', 'NO':'NO_new', 'NO2':'NO2_new', 'SO2':'SO2_new','NOX':'NOX_new','NOY':'NOY_new','TEMP':'TEMP2','WS':'WSPD10','WD':'WDIR10','SRAD':'GSW','BARPR':'PRSFC','PRECIP':'RT','RHUM':'Q2'}
+    sub_map  = {i: mapping_table[i] for i in species if i in mapping_table}
+    sub_maps = pd.Series(sub_map)
+    print('species...')
+    print(sub_maps)
     print('combining paired files...')
     print(paired_files)
+
     count=0
     for i in paired_files:
         df=pd.read_hdf(i) 
@@ -75,17 +88,32 @@ if __name__ == '__main__':
                 df_merge=df
         else: 
                 if mergedf is False:
-                	df_species=df[species]
+                	df_species = df[sub_maps]
                 	df_species = df_species.add_suffix('_'+str(count+1))
-                	print('joining model data columns...')
-                	df_merge=df_merge.join(df_species)
+                	if avg is True:
+                        	print('averaging dataframes, both obs and mod columns (e.g., for multiple years)...')
+                        	df_species_obs = (df[sub_map]+df_merge[sub_map])/2. #get observations column(s) ave
+                        	#df_species_obs = df_species_obs.add_suffix('_ave'+str(count+1))
+                        	df_species_mod = (df[sub_maps]+df_merge[sub_maps])/2. # get model column(s) ave
+                        	#df_species_mod = df_species_mod.add_suffix('_ave'+str(count+1))
+                        	df_merge=df_merge.drop(sub_map,axis=1) #drop prev obs columns
+                        	df_merge=df_merge.drop(sub_maps,axis=1) #drop old mod columns
+                        	df_merge=df_merge.join(df_species_obs) #merge observation column(s)
+                        	df_merge=df_merge.join(df_species_mod) #merge model column(s)
+                       		#df_merge=df_merge.drop(sub_map,axis=1) #drop old obs columns
+                        	#df_merge=df_merge.drop(sub_maps,axis=1) #drop old mod columns
+                        	#df_merge.columns=df_merge.columns.str.strip(to_strip='_ave'+str(count+1)) #strip average suffixes
+                	else:
+               			print('joining dataframes, just add mod columns (e.g., for multiple model runs)...')
+                		df_merge=df_merge.join(df_species)
                 else:
-                	df[species] = df[species].add_suffix('_'+str(count+1))
+                	df[species] = df[sub_maps].add_suffix('_'+str(count+1))
                 	print('merging entire dataframes...slow...')
                 	df_merge=df_merge.merge(df,on='time')
         count=count+1
     print('final merged data frame...')
     print(df_merge.keys())
+    print(df_merge)
 
     df_merge.to_hdf(output+'.hdf',
             'df_merge',
